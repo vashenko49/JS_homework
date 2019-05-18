847e7e5fb19c317f760adc964b5626dcfe69ae47
Sergey Vashchenko <vashenko49@gmail.com>
2019-05-18 15:43:46 +0300

    redid 2

--- homework1/js/script.js
+++ homework1/js/script.js
@@ -1,20 +1,20 @@
-function check_enter_name(name = "Name") {
+function checkEnterName(name = "Name") {
     name = prompt("Enter your name",name);
     if (!name){
-        name = check_enter_name(name);
+        name = checkEnterName(name);
     }
     return name;
 }
-function check_enter_year(years = "Years") {
-    years = prompt("How are you years?", years );
+function checkEnterYear(years = "Years") {
+    years = +prompt("How are you years?", years );
     if (isNaN(years)){
-        years = check_enter_year(years);
+        years = checkEnterYear(years);
     }
     return years;
 }
 
-let name = check_enter_name();
-let years = check_enter_year();
+let name = checkEnterName();
+let years = checkEnterYear();
 
 if (years<18){
     alert("You are not allowed to visit this website")
