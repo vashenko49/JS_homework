23eb15730dc86bc98b5d4594fc56cd8d90a8510e
Sergey Vashchenko <vashenko49@gmail.com>
2019-06-01 13:23:17 +0300

    create HW 4

--- homework2/js/script.js
+++ homework2/js/script.js
@@ -1,5 +1,5 @@
-function enterNumber(number = "Number") {
-    number = prompt("Enter your number multiples of five", number);
+function enterNumber(number = "Number",textForPromt="Enter") {
+    number = prompt(textForPromt, number);
     number = +number.replace(',','.');//сделал так потому что когда пользователь введет дробное число, число не проходит проверку и передается рекурсии NaN
     if (isNaN(number) || !Number.isInteger(number)){
         number = enterNumber(number);
@@ -23,8 +23,7 @@ function multiplesFive(start, finish) {
 function simpleNumber(start = 2, finish=3){
     //инкапсулировал функцию, что бы ее нетрогали из внешки
     function isPrime(temp){
-        let max=Math.floor(Math.sqrt(temp));
-        for (let i = 2; i <= max; i++) {
+        for (let i = 2; i <= Math.floor(Math.sqrt(temp)); i++) {
             if (temp%i===0) {
                 return false;
             }
@@ -35,7 +34,7 @@ function simpleNumber(start = 2, finish=3){
     let listNumber = [];
     if(start<2||finish<2 || start>=finish|| !isFinite(start) || start%1 || !isFinite(finish) || finish%1  ){
         alert('No valid');
-        simpleNumber(enterNumber('from'),enterNumber('to'));
+        simpleNumber(enterNumber('from',"Enter again"),enterNumber('to',"enter again"));
     }
     else {
         for (;start<=finish;start++){
@@ -51,6 +50,6 @@ function simpleNumber(start = 2, finish=3){
 }
 
 //кратные 5
-console.log(multiplesFive(enterNumber('from'),enterNumber('to')));
+console.log(multiplesFive(enterNumber('from','Enter from '),enterNumber('to', "Enter to")));
 //простые числа
-console.log(simpleNumber(enterNumber('from'),enterNumber('to')));
+console.log(simpleNumber(enterNumber('from',"Enter from"),enterNumber('to',"Enter to")));
---
+++ homework4/js/script.js
@@ -0,0 +1,36 @@
+function enterString(message="Enter",tempString = "Enter") {
+    tempString = prompt(message,tempString);
+    if(!tempString){
+        tempString = enterString(message + " again",tempString);
+    }
+    return tempString;
+}
+
+function createUser(firstName, lastName) {
+    const newUser = {};
+
+    Object.defineProperties(newUser,{
+       "firstName":{
+           value:firstName,
+           configurable:true,
+           writable:false
+       },
+        "lastName":{
+            value:lastName,
+            configurable:true,
+            writable:false
+        }
+    });
+
+    newUser.setLastName= function (tempLast){
+      Object.defineProperty(newUser,"lastName",{value:tempLast});
+    };
+
+    newUser.setFirstName= function (temFirst){
+        Object.defineProperty(newUser,"firstName",{value:temFirst});
+    };
+    return newUser;
+}
+
+let test = createUser(enterString('enter firstname'),enterString('enter lastname'));
+
