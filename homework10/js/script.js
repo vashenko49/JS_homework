372360eef9bf6b9e9e0f1ffa28ee8913ce853e9f
Sergey Vashchenko <vashenko49@gmail.com>
2019-06-26 02:02:17 +0300

    redid HW10

--- homework10/js/script.js
+++ homework10/js/script.js
@@ -1,12 +1,11 @@
-//проверяет наличие класса в елементе, если находит меняет его на новый
+/*region with show or hide password*/
 function checkAndReplaysClassName (element, showPassword, hidePassword){
-    debugger;
     if(element.classList.contains(hidePassword)){
         element.classList.remove(hidePassword);
         element.classList.add(showPassword);
         return true; //если пароль должен отображаться возращаеться true
     }
-    else if(element.classList.contains(showPassword)){
+    else{
         element.classList.remove(showPassword);
         element.classList.add(hidePassword);
         return false; //если пароль должен скрыться то отображение убереться
@@ -16,40 +15,31 @@ function checkAndReplaysClassName (element, showPassword, hidePassword){
 document.addEventListener('click', function(event) {
    let target = event.target;
    if(target.hasAttribute('data-show-password')){
-        if (checkAndReplaysClassName(target,'fa-eye-slash','fa-eye')){
-            target.previousElementSibling.type = 'text';
-        }
-        else {
-            target.previousElementSibling.type = 'password';
-        }
+       checkAndReplaysClassName(target,'fa-eye-slash','fa-eye')?target.previousElementSibling.type = 'text':target.previousElementSibling.type = 'password';
    }
 });
+/*end region with show or hide password*/
 
-
-
+/*region with compare password*/
 let inputWrapper = document.querySelectorAll('.input-wrapper');
 let form = document.getElementById('form');
-
-
-
+/*модальное окно при успехе*/
 function inputPasswordSuccessful(){
     let response = document.createElement('div');
     response.classList.add('bg-modal-window');
     response.addEventListener('click',function (event) {
         let target = event.target;
-       if(target ===response){
+        if(target ===response){
            target.remove();
-       }
+        }
     });
     let message = document.createElement('p');
     message.classList.add('modal-window');
     message.innerHTML= 'You are welcome';
     response.append(message);
     return response;
-
 }
-
-
+/*дабавляет строку уведомляя про неудачу*/
 function inputPasswordFail(){
     let response = document.createElement('p');
     response.innerHTML = 'Нужно ввести одинаковые значения';
@@ -57,32 +47,13 @@ function inputPasswordFail(){
     response.style.marginTop = '0';
     return response;
 }
-
-
-
-function checkPassword(elementToCheck){
-    for(let i =0;i< elementToCheck.length;i++){
-        if(elementToCheck[0].firstElementChild.value!==elementToCheck[i].firstElementChild.value || !elementToCheck[i].firstElementChild.value){
-            return  false;
-        }
-    }
-    return true;
-}
-
-
-
 let responseToUser;
 form.addEventListener('submit',function (event) {
     if(responseToUser){
         responseToUser.remove();
     }
-
-    if(checkPassword(inputWrapper)){
-        responseToUser = inputPasswordSuccessful();
-    }
-    else {
-        responseToUser = inputPasswordFail();
-    }
+    (inputWrapper[0].firstElementChild.value === inputWrapper[1].firstElementChild.value && inputWrapper[1].firstElementChild.value && inputWrapper[0].firstElementChild.value)? responseToUser = inputPasswordSuccessful():responseToUser = inputPasswordFail();
     form.insertBefore(responseToUser,form.lastElementChild);
     event.preventDefault();
 });
+/*end region with compare password*/
