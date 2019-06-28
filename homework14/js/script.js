bb950873641ea4d918641b5a98ecf63c1bb1bfc1
Sergey Vashchenko <vashenko49@gmail.com>
2019-06-29 01:15:04 +0300

    create HW14

---
+++ homework14/js/script.js
@@ -0,0 +1,14 @@
+let $tabs =null,
+    $tabsInformation =null;
+
+window.onload=function () {
+   $tabs = $('.tabs-title');
+   $tabsInformation = $(".tabs-content>li");
+   $tabs.click(function (event) {
+      let $target = $(event.target);
+      $('.active').removeClass('active');
+      $target.addClass('active');
+      $('.activeText').removeClass('activeText');
+      $tabsInformation.eq($target.index()).addClass('activeText');
+   });
+};
