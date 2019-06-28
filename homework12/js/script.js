c1cd944e34f8e64bf3e358bab9c3ebfa1c5eb155
Sergey Vashchenko <vashenko49@gmail.com>
2019-06-28 15:02:21 +0300

    redid hq12

--- homework12/js/script.js
+++ homework12/js/script.js
@@ -1,86 +1,76 @@
-function hideOrShowButtonContinue(hideOrShow){
-    if(hideOrShow){
-        document.getElementById('continue').style.display = 'inline-block';
-    }
-    else {
-        document.getElementById('continue').style.display = 'none';
-    }
+let $btnContinue =null,
+    timeBegan = null,
+    currentImg =0,
+    $allImg = null,
+    $displayArea = null,
+    timeStopped = null,
+    stoppedDuration = 0,
+    started = null;
+
+/*скрытие кнопки продолжить*/
+function hideOrShowButtonContinue(hideBtn){
+    return hideBtn?$btnContinue.css({display:'inline-block'}):$btnContinue.css({display:'none'});
 }
-
-$('#stop').click(function () {
-   stop();
-   hideOrShowButtonContinue(true);
-});
-$('#continue').click(function () {
-   start();
-   hideOrShowButtonContinue(false);
-});
-
-let currentImg=0;
-let $imd = $('.image-to-show');
 
 function getHideIndexSetCurrentImg() {
     let old = currentImg;
-    if(currentImg>=$imd.length-1){
-        currentImg=0;
-    }
-    else {
-        currentImg++;
-    }
-    if(currentImg===0){
-        old = $imd.length-1;
-    }
-
+    (currentImg>=$allImg.length-1)?currentImg=0:currentImg++;
+    if(currentImg===0)
+        old = $allImg.length-1;
     return old;
 }
 
-let timeBegan = null
-    , timeStopped = null
-    , stoppedDuration = 0
-    , started = null;
-
-function start() {
-    if (timeBegan === null) {
+function startTime() {
+    if(timeBegan===null)
         timeBegan = new Date();
-    }
-    $imd.eq(currentImg).delay(500).fadeIn(500);
-    if (timeStopped !== null) {
-        stoppedDuration += (new Date() - timeStopped);
-    }
-    console.log(stoppedDuration);
 
-    started = setInterval(clockRunning, 10);
+    //появдение картинки
+    $allImg.eq(currentImg).delay(500).fadeIn(500);
+
+    if(timeStopped!==null)
+        stoppedDuration+=(new Date()-timeStopped);
+
+    started = setInterval(runTime,10);
 }
-
-function stop() {
-    timeStopped = new Date();
+function stopTime() {
+    timeStopped= new Date();
     clearInterval(started);
 }
 
-function reset() {
+function resetTime() {
     clearInterval(started);
-    stoppedDuration = 0;
-    timeBegan = null;
+    stoppedDuration =0;
+    timeBegan =null;
     timeStopped = null;
-    document.getElementById("display-area").innerHTML = ":00.000";
-}
 
-function clockRunning(){
-    let currentTime = new Date()
-        , timeElapsed = new Date(currentTime - timeBegan - stoppedDuration)
-        , sec = timeElapsed.getUTCSeconds()
-        , ms = timeElapsed.getUTCMilliseconds();
-    if(sec>=10){
-        reset();
-        $imd.eq(getHideIndexSetCurrentImg()).fadeOut(500);
-        start();
+}
+function runTime() {
+    let currentTime = new Date(),
+        timeRunOut = new Date(currentTime - timeBegan - stoppedDuration),
+        sec = timeRunOut.getUTCSeconds(),
+        ms = timeRunOut.getUTCMilliseconds();
+    if (sec >= 10) {
+        resetTime();
+        //показ картинки
+        $allImg.eq(getHideIndexSetCurrentImg()).fadeOut(500);
+        startTime();
     }
-    document.getElementById("display-area").innerHTML =
-        (sec > 9 ? sec : "0" + sec) + "." +
-        (ms > 99 ? ms : ms > 9 ? "0" + ms : "00" + ms);
+    $displayArea.html(`${sec > 9 ? sec : "0" + sec}.${(ms > 99 ? ms : ms > 9 ? "0" + ms : "00" + ms)}`);
 }
 
 window.onload = function () {
+    $btnContinue = $('#continue');
+    $btnContinue.click(function () {
+        startTime();
+        hideOrShowButtonContinue(false);
+    });
+    $('#stop').click(function () {
+        stopTime();
+        hideOrShowButtonContinue(true);
+    });
+    $allImg = $('.image-to-show');
+    $displayArea = $('#showTime');
+    $('.image-to-show:eq(0)').css('display','block');
     hideOrShowButtonContinue(false);
-  start();
+    startTime();
 };
