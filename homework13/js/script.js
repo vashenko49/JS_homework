64f9758a36b56441e17839982d0e76c4a7e9cf1b
Sergey Vashchenko <vashenko49@gmail.com>
2019-06-28 16:21:41 +0300

    create HW13

---
+++ homework13/js/script.js
@@ -0,0 +1,16 @@
+let $linkOnChange =null;
+
+window.onload=function () {
+    $linkOnChange = $('#changeTheme');
+    if(localStorage.getItem('countTheme')===null){
+        localStorage.setItem('countTheme','0');
+    }
+    setTheme();
+    $('#changeMode').click(function () {
+        localStorage.getItem('countTheme')==='0'?localStorage.setItem('countTheme',"1"):localStorage.setItem('countTheme',"0");
+        setTheme()
+    });
+};
+function setTheme() {
+    localStorage.getItem('countTheme')==='0'? $linkOnChange.attr('href','css/regular_version.css'):$linkOnChange.attr('href','css/modified_version.css');
+}
